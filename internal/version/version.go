6e41e5d5e0acab8517ff83f5b65ff0636da668ad
carrol <hexiang05@bilibili.com>
2026-03-12 09:45:08 +0800

    chore(ver): version fix: 0.26.2-ds.20260311 -> 0.26.2-ds.20260312

--- internal/version/version.go
+++ internal/version/version.go
@@ -9,7 +9,7 @@ import (
 
 // Version is the service current released version.
 // Semantic versioning: https://semver.org/
-var Version = "0.26.2-ds.20260311"
+var Version = "0.26.2-ds.20260312"
 
 func GetCurrentVersion() string {
 	return Version
